e6a6c9a9553d8442c76a2fa36acdefcf7b8afae1
bichanna <nobu.bichanna@gmail.com>
2022-01-09 12:36:11 -0600

    Create partial parser

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,4 @@
-from token import TokenType
+from token import Token, TokenType
 
 class NumberNode:
 	def __init__(self, token):
@@ -18,3 +18,39 @@ class BinOpNode:
 		return f"({self.left_node}, {self.op_token}, {self.right_node})"
 
 
+class Parser:
+	def __init__(self, tokens):
+		self.tokens = tokens
+		self.token_index = -1
+		self.advance()
+	
+	def advance(self):
+		self.token_index += 1
+		if self.token_index < len(self.tokens):
+			self.current_token = self.tokens[self.token_index]
+		return self.current_token
+	
+	def factor(self):
+		token = self.current_token
+		if token.type in (TokenType.INT, TokenType.FLOAT):
+			self.advance()
+			return NumberNode(token)
+
+	def term(self):
+		return self.bin_op_node(self.factor, (TokenType.MULT, TokenType.DIV))
+	
+	def expr(self):
+		return self.bin_op_node(self.term, (TokenType.PLUS, TokenType.MINUS))
+	
+	def bin_op_node(self, func, ops):
+		left = func()
+		while  self.current_token in ops:
+			op_token = self.current_token
+			right = func()
+			# self.advance()
+			left = BinOpNode(left, op_token, right)
+		return left
+	
+	def parse(self):
+		result = self.expr()
+		return result
