942ccb67853a1a04521b1a1cb6e194c18f9aa4a4
bichanna <nobu.bichanna@gmail.com>
2022-01-09 11:23:25 -0600

    Modify Error class to accomodate Position class

--- src/error.py
+++ src/error.py
@@ -1,5 +1,7 @@
 class Error:
-	def __init__(self, error_name, details):
+	def __init__(self, pos_start, pos_end, error_name, details):
+		self.pos_start = pos_start
+		self.pos_end = pos_end
 		self.error_name = error_name
 		self.details = details
 	
@@ -8,5 +10,5 @@ class Error:
 		return f"Gen::{self.error_name}: {self.details}"
 
 class TypeCharError(Error):
-	def __init__(self, details):
-		super().__init__("TypeCharError", details)
+	def __init__(self, pos_start, pos_end, details):
+		super().__init__(pos_start, pos_end,"TypeCharError", details)
