9ada7081b4e7ede3154d5e4ede272f15309428e2
bichanna <nobu.bichanna@gmail.com>
2022-01-09 11:10:59 -0600

    Add run function

--- src/gen.py
+++ src/gen.py
@@ -1,4 +1,17 @@
+from lexer import Lexer
 
-while True:
-	t = input("gen>> ")
-	print(t)
+def run(text):
+	lexer = Lexer(text)
+	tokens, err = lexer.make_tokens()
+
+	return tokens, err
+
+def main():
+	while True:
+		t = input("gen>> ")
+		result, err = run(t)
+		if err is not None: print(err.as_string)
+		else: print(result)
+
+if __name__ == "__main__":
+	main()
