22e60f9af36bdabe54f1ded87968f645fce90327
bichanna <nobu.bichanna@gmail.com>
2022-01-10 09:53:40 -0600

    Fix non-def error

--- parser.py
+++ parser.py
@@ -4,6 +4,8 @@ from error import InvalidSyntaxError
 class NumberNode:
 	def __init__(self, token):
 		self.token = token
+		self.pos_start = self.token.pos_start
+		self.pos_end = self.token.pos_end
 
 	def __repr__(self):
 		return f"{self.token}"
@@ -14,6 +16,9 @@ class BinOpNode:
 		self.left_node = left_node
 		self.op_token = op_token
 		self.right_node = right_node
+		
+		self.pos_start = self.left_node.pos_start
+		self.pos_end = self.right_node.pos_end
 	
 	def __repr__(self):
 		return f"({self.left_node}, {self.op_token}, {self.right_node})"
@@ -23,6 +28,8 @@ class UnaryOpNode:
 	def __init__(self, op_token, node):
 		self.op_token = op_token
 		self.node = node
+		self.pos_start = self.op_token.pos_start
+		self.pos_end = self.node.pos_end
 	
 	def __repr__(self):
 		return f"({self.op_token}, {self.node})"
