61457d1229387c111f2097a11039287638a4e014
bichanna <nobu.bichanna@gmail.com>
2022-01-09 22:39:34 -0600

    Fix lexer

--- lexer.py
+++ lexer.py
@@ -26,19 +26,19 @@ class Lexer:
 			elif self.current_char in DIGITS:
 				tokens.append(self.make_number())
 			elif self.current_char == "+":
-				tokens.append(tk.TT_PLUS)
+				tokens.append(tk.Token(tk.TT_PLUS, pos_start=self.position))
 				self.advance()
 			elif self.current_char == "*":
-				tokens.append(tk.TT_MULT)
+				tokens.append(tk.Token(tk.TT_MULT, pos_start=self.position))
 				self.advance()
 			elif self.current_char == "/":
-				tokens.append(tk.TT_DIV)
+				tokens.append(tk.Token(tk.TT_DIV, pos_start=self.position))
 				self.advance()
 			elif self.current_char == "(":
-				tokens.append(tk.TT_L_PAREN)
+				tokens.append(tk.Token(tk.TT_L_PAREN, pos_start=self.position))
 				self.advance()
 			elif self.current_char == ")":
-				tokens.append(tk.TT_R_PAREN)
+				tokens.append(tk.Token(tk.TT_R_PAREN, pos_start=self.position))
 				self.advance()
 			else:
 				# return TypeCharError
@@ -46,11 +46,13 @@ class Lexer:
 				char = self.current_char
 				self.advance()
 				return [], TypeCharError(pos_start, self.position, f"'{char}'")
+		tokens.append(tk.Token(tk.TT_EOF), pos_start=self.position)
 		return tokens, None
 	
 	def make_number(self):
 		number_str = ""
 		dot = False
+		pos_start = self.position.copy()
 		while self.current_char is not None and self.current_char in DIGITS + ".":
 			if self.current_char == ".":
 				if dot is True: break
@@ -60,4 +62,4 @@ class Lexer:
 				number_str += self.current_char
 			self.advance()
 		
-		return tk.Token(tk.TT_INT, int(number_str)) if dot is False else tk.Token(tk.TT_FLOAT, float(number_str))
+		return tk.Token(tk.TT_INT, int(number_str), pos_start, self.position) if dot is False else tk.Token(tk.TT_FLOAT, float(number_str), pos_start, self.position)
