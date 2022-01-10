d7995fa99ba86c07c353691b45b23b63ac3f349e
bichanna <nobu.bichanna@gmail.com>
2022-01-10 09:36:46 -0600

    Create Number class and add_to method

--- evaluator.py
+++ evaluator.py
@@ -1,3 +1,19 @@
+class Number:
+	def __init__(self, value):
+		self.value = value
+		self.set_position()
+	
+	def set_position(self, pos_start=None, pos_end=None):
+		self.pos_start = pos_start
+		self.pos_end = pos_end
+		return self
+	
+	def add_to(self, other):
+		# This language will have strings and arrays, so check
+		# if the other is Number or not is necessary later
+		if isinstance(other, Number):
+			return Number(self.value + other.value)
+
 class Evaluator:
 	def visit(self, node):
 		method_to_be_called = f"visit_{type(node).__name__}"
