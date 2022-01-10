461f92b2907c1afd27fd86387bcf7042a0a14122
bichanna <nobu.bichanna@gmail.com>
2022-01-09 21:34:43 -0600

    Update token class

--- token.py
+++ token.py
@@ -10,9 +10,13 @@ TT_R_PAREN	= "R_PAREN"
 
 
 class Token:
-	def __init__(self, type, value=None):
+	def __init__(self, type, value=None, pos_start=None, pos_end=None):
 		self.type = type
 		self.value = value
+		if pos_start is not None:
+			self.pos_start = pos_start.copy()
+			self.pos_end = pos_start.copy()
+			self.pos_end.advance()
 	
 	def __repr__(self):
 		return f"{self.type}:{self.value}" if self.value is not None else self.type
