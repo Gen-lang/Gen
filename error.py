782a4e99189f26d38f23bdcaf20bacc4c18e589b
bichanna <nobu.bichanna@gmail.com>
2022-01-10 09:59:42 -0600

    Add RuntimeError

--- error.py
+++ error.py
@@ -21,3 +21,7 @@ class TypeCharError(Error):
 class InvalidSyntaxError(Error):
 	def __init__(self, pos_start, pos_end, details=""):
 		super().__init__(pos_start, pos_end, "InvalidSyntaxError", details)
+
+class RuntimeError(Error):
+	def __init__(self, pos_start, pos_end, details=""):
+		super().__init__(pos_start, pos_end, "RuntimeError", details)
