452709cf3978f9fc16356826de4f0651053e5f9e
bichanna <nobu.bichanna@gmail.com>
2022-01-09 21:30:39 -0600

    Add string_with_arrows func to the error message

--- error.py
+++ error.py
@@ -1,3 +1,5 @@
+from utils import string_with_arrows
+
 class Error:
 	def __init__(self, pos_start, pos_end, error_name, details):
 		self.pos_start = pos_start
@@ -9,8 +11,13 @@ class Error:
 	def as_string(self):
 		string = f"File {self.pos_start.filename}, line {self.pos_start.lnum+1}\n"
 		string += f"Gen::{self.error_name}: {self.details}"
+		string += f"\n\n" + string_with_arrows(self.pos_start.filetext, self.pos_start.pos_start, self.pos_end)
 		return string
 
 class TypeCharError(Error):
 	def __init__(self, pos_start, pos_end, details):
-		super().__init__(pos_start, pos_end,"TypeCharError", details)
+		super().__init__(pos_start, pos_end, "TypeCharError", details)
+
+class InvalidSyntaxError(Error):
+	def __init__(self, pos_start, pos_end, details=""):
+		super().__init__(pos_start, pos_end, "InvalidSyntaxError", details)
