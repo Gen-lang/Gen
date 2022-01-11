4e7a104ccd970b8c5b72326ef8fe0645c3ec210b
bichanna <nobu.bichanna@gmail.com>
2022-01-10 21:54:47 -0600

    Create VarAccessNode and VarAssignNode

--- node.py
+++ node.py
@@ -30,3 +30,18 @@ class UnaryOpNode:
 	
 	def __repr__(self):
 		return f"({self.op_token}, {self.node})"
+
+
+class VarAccessNode:
+	def __init__(self, var_name_token):
+		self.var_name_token = var_name_token
+		self.pos_start = self.var_name_token.pos_start
+		self.pos_end = self.var_name_token.pos_end
+
+
+class VarAssignNode:
+	def __init__(self, var_name_token, value_node):
+		self.var_name_token = var_name_token
+		self.value_node = value_node
+		self.pos_start = self.var_name_token.pos_start
+		self.pos_end = self.value_node.pos_end
