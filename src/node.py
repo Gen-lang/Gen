3eaf6e689e25c16357000a380db430d1715aca5c
bichanna <nobu.bichanna@gmail.com>
2022-01-12 00:37:07 -0600

    Add ForNode

--- src/node.py
+++ src/node.py
@@ -53,3 +53,16 @@ class IfNode:
 		self.else_case = else_case
 		self.pos_start = self.cases[0][0].pos_start
 		self.pos_end = (self.else_case or self.cases[len(self.cases)-1][0]).pos_end
+
+
+class ForNode:
+	def __init__(self, var_name_token, start_value_node, end_value_node, step_value_node, body_node):
+		self.var_name_token = var_name_token
+		self.start_value_node = start_value_node
+		self.end_value_node = end_value_node
+		self.step_value_node = step_value_node
+		self.body_node = body_node
+		self.pos_start = self.var_name_token.pos_start
+		self.pos_end = self.body_node.pos_end
+
+
