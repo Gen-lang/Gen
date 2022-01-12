a1ea71fe9cdf6ee4c346ea3fd83cddd1db72ce1f
bichanna <nobu.bichanna@gmail.com>
2022-01-11 22:25:05 -0600

    Create IfNode

--- src/node.py
+++ src/node.py
@@ -45,3 +45,11 @@ class VarAssignNode:
 		self.value_node = value_node
 		self.pos_start = self.var_name_token.pos_start
 		self.pos_end = self.value_node.pos_end
+
+
+class IfNode:
+	def __init__(self, cases, else_case):
+		self.cases = cases
+		self.else_case = else_case
+		self.pos_start = self.cases[0][0].pos_start
+		self.pos_end = (self.else_case or self.cases[len(self.cases)-1][0]).pos_end
