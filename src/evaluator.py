032cd3262b430359936911ae00a79225a8809c40
bichanna <nobu.bichanna@gmail.com>
2022-01-13 10:40:41 -0600

    Add is_true method for string

--- src/evaluator.py
+++ src/evaluator.py
@@ -194,6 +194,9 @@ class String(Value):
 		else:
 			return None, Value.invalid_operation(self.pos_start, other.pos_end)
 	
+	def is_true(self):
+		return len(self.value) > 0
+	
 	def copy(self):
 		copy = String(self.value)
 		copy.set_position(self.pos_start, self.pos_end)
