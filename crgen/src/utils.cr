42115b58c8dc53a5919f4c9aab810bd27dc773b3
bichanna <nobu.bichanna@gmail.com>
2022-01-18 16:39:03 -0600

    Remove Crystal files

--- crgen/src/utils.cr
+++
@@ -1,7 +0,0 @@
-def string_with_arrows(text : String, pos_start, pos_end) 
-	# I know this sucks. I will make it better some day.
-	result = text.gsub("\t") {""}
-	result += "\n"
-	result += ("^" * text.size)
-	return result
-end
