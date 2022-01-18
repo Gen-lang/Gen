07009aacb50de8fa3fa08ba4e09c5c128e6fe455
bichanna <nobu.bichanna@gmail.com>
2022-01-18 16:17:29 -0600

    Add static-typing to Context class

--- crgen/src/Context.cr
+++ crgen/src/Context.cr
@@ -1,5 +1,7 @@
+require "./Position"
+
 class Context
-	def initialize(display_name, parent=nil, parent_entry_pos=nil)
+	def initialize(display_name : String, parent : Context = nil, parent_entry_pos : Position = nil)
 		@display_name = display_name
 		@parent = parent
 		@parent_entry_pos = parent_entry_pos
