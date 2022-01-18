071727a68a2603e1087b7711840ceb6d68011a34
bichanna <nobu.bichanna@gmail.com>
2022-01-17 21:26:50 -0600

    Create Position class in Crystal

---
+++ crgen/src/Position.cr
@@ -0,0 +1,22 @@
+class Position
+	def initialize(index : Int32, lnum : Int32, col : Int32, filename : String, filetext : String)
+		@index = index
+		@lnum = lnum
+		@col = col
+		@filename = filename
+		@filetext = filetext
+	end
+
+	def advance(current_char=Nil)
+		@index += 1
+		@col += 1
+		if current_char == "\n"
+			@lnum += 1
+			@col = 0
+		end
+	end
+
+	def copy()
+		return Position.new(@index, @lnum, @col, @filename, @filetext)
+	end
+end
