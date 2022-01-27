8bc952e52903c9d1995fdc88dfb3ae42da87c206
bichanna <nobu.bichanna@gmail.com>
2022-01-27 00:33:36 -0600

    Fix the typo

--- pygen/src/node.py
+++ pygen/src/node.py
@@ -129,12 +129,15 @@ class ForNode:
 
 class ForInNode:
 	def __init__(self, var_name_token, array_elements, body_node, should_return_null):
-		self.var_name = var_name_token
+		self.var_name_token = var_name_token
 		self.array_elements = array_elements
 		self.body_node = body_node
 		self.pos_start = self.var_name_token.pos_start
 		self.pos_end = self.body_node.pos_end
 		self.should_return_null = should_return_null
+	
+	def __repr__(self):
+		return f"{self.var_name_token}\n{self.array_elements}"
 
 
 class WhileNode:
